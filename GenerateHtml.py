83526ab1c1340e66880bdb6e78ae35b25a1d928e
nikminer <nikminerfeed@gmail.com>
2019-04-26 01:20:59 +0300

    test

--- GenerateHtml.py
+++ GenerateHtml.py
@@ -1,4 +1,9 @@
 import os
-index=open('index.html',"w")
+from urllib.parse import unquote
+index=open('index.html',"w", encoding='utf-8')
 
 index.write(open('templates/header.html').read())
+
+for i in os.listdir("src\\"):
+        if (not os.path.isdir("src\\"+i)):
+                index.write('<img src=\"src\\'+i+'\">')
